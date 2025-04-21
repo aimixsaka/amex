14b943cbc05d941fe62f791df40eddfd16978c1f
aimixsaka <aimixsaka@gmail.com>
2025-04-21 15:20:57 +0800

    fix(typo): fix comment in debug.h

--- src/debug.h
+++ src/debug.h
@@ -6,16 +6,16 @@
 #ifdef DEBUG
 #define	DEBUG_TRACE_EXECUTION
 #define DEBUG_PRINT_CODE
-/*
- * if this flag on, gc start at every `reallocate`,
- * bad for performace, good for debug gc
- * related memory management butgs.
- */
 #endif /* DEBUG */
 
 #ifdef DEBUG_GC
 #define	DEBUG_TRACE_EXECUTION
 #define DEBUG_PRINT_CODE
+/*
+ * if this flag on, gc start at every `reallocate`,
+ * bad for performace, good for debugging gc
+ * related memory management bugs.
+ */
 #define DEBUG_STRESS_GC
 #define DEBUG_LOG_GC
 #endif /* DEBUG_GC */
